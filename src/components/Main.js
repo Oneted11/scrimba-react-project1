5c13e5e3a2a02a97c4d0b28f9372b19fc426a518
Oneted11 <muriithited@gmail.com>
2022-06-08 09:35:47 +0300

    feat:switched to marker from before

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ export default function Main() {
     <article className="main-content">
       <h1>Fun facts about React</h1>
       <ul>
-        <li>Was first release in 2013</li>
+        <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
         <li>Is maintained by Facebook</li>
